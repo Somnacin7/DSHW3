1cb61c631f0d367a4aa4e6fe7af5672b84421d30
Robin Steyer <robinmarie051@yahoo.com>
2015-06-17 19:28:24 -0400

    Added class structure

--- DSHW3/HafnerSteyer3.cpp
+++ DSHW3/HafnerSteyer3.cpp
@@ -1,5 +1,31 @@
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Email {
+	string to;
+	string from;
+	string message;
+
+	Email* newer;
+	Email* older;
+};
+
+class Communication {
+	string subject;
+	int numOfEmails;
+	Email* newestEmail;
+
+	Communication* newer;
+	Communication* older;
+};
 
 class Inbox {
 
 };
+
+int main() {
+	return 0;
+}
