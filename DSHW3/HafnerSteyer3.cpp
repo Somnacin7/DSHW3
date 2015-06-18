3bfbcb85646b83c8dd290db052acbd8a03b05cdf
Robin Steyer <robinmarie051@yahoo.com>
2015-06-18 13:18:22 -0400

    added some function definitions and comments

--- DSHW3/HafnerSteyer3.cpp
+++ DSHW3/HafnerSteyer3.cpp
@@ -4,18 +4,26 @@
 
 using namespace std;
 
+// Email class
 class Email {
 private:
 	string to;
 	string from;
 	string message;
 
+public:
+	// constructor
+	Email(string to, string from, string message) {
+		this->to = to;
+		this->from = from;
+		this->message = message;
+	}
+
 	Email* newer;
 	Email* older;
-
-public:
 };
 
+// Communication class
 class Communication {
 private:
 	string subject;
@@ -24,23 +32,91 @@ private:
 
 	Communication* newer;
 	Communication* older;
+
 public:
+	// constructor
+	Communication(string subject, Email* email) {
+		this->subject = subject;
+		newestEmail = email;
+		newestEmail->newer = NULL;
+		newestEmail->older = NULL;
+	}
+
+	void InsertEmail(Email* email) {
+		email->older = newestEmail;
+		newestEmail = email;
+		newestEmail->newer = NULL;
+	}
+
+	string getSubject() {
+		return subject;
+	}
+
+	Communication* getNewer() {
+		return newer;
+	}
+
+	Communication* getOlder() {
+		return older;
+	}
 };
 
+// Inbox class
 class Inbox {
-	void InsertEmail(Email*);
-
-	bool DeleteCommunication(string subject);
-
-	void DisplayInbox();
-	
 private:
-	Communication* SearchCommunicaton(string subject);
+	Communication* SearchCommunicaton(string subject) {
+		Communication* comm = top;
+		while (comm != NULL) {
+			if (comm->getSubject() == subject) {
+				return comm;
+			}
+			comm = comm->getOlder();
+		}
+		return NULL;
+	}
 
+	Communication* top;
+	Communication* bottom;
 
+public:
+	void InsertEmail(Email* email, string subject) {
 
+		// search for a communicatino with the given subject to place the email in
+		Communication* comm = SearchCommunicaton(subject);
+		if (comm != NULL) {
+			comm->InsertEmail(email);
+		}
+		else {
+			// create a new communication
+		}
+	}
+
+	bool DeleteCommunication(string subject) {
+
+	}
+
+	void DisplayInbox() {
+
+	}
 };
 
 int main() {
+	string subject = "";
+	Inbox myInbox;
+	Email mail("recipient", "sender", "text");
+	Communication comm((string)"sub", &mail);
+
+	cout << "Enter email subjects one at a time (press enter after each one)." << endl;
+	cout << "When you are finished, enter the word 'done'" << endl << endl;
+	
+	// get subjects from user
+	while (getline(cin, subject) && subject != "done") {
+		myInbox.InsertEmail(&mail, subject);
+		
+	}
+
+	//display inbox
+	myInbox.DisplayInbox();
+
 	return 0;
 }
